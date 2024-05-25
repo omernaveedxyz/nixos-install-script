2dae3e65f12d57bd98646ac674a998a547e72f64
Omer Naveed <contact@mail.omernaveed.dev>
2024-05-25 00:26:38 -0500

    package.nix: remove build inputs
    Remove build inputs from Nix package.
    
    In scenarios where the installation script is used on a machine with
    limited disk and memory space (i.e. a virtual machine), there would not
    be enough space present for all of the build inputs to be installed and
    therefore the installation script could not be compiled.
    
    However, the build inputs are already included in the installation ISO
    and therefore do not **need** to be installed in order for the
    installation script to work. The only problems that may arise are
    
    	1. The ISO used does not have one/some of the programs present
    	2. The version of the program that is installed in the ISO is
    	   older than what is in the flake.lock nixpkgs version **and**
    	   missing some options that this script uses.
    
    In order to resolve the first issue, a check can be added to the start
    of the program in order to check that the programs are all present. The
    second issue is more difficult to resolve.

--- package.nix
+++ package.nix
@@ -13,29 +13,8 @@ in
     # The package source directory
     src = ./src;
 
-    # The package's build-time dependencies
-    nativeBuildInputs = with pkgs; [makeWrapper];
-
-    # The package's run-time dependencies
-    buildInputs = with pkgs; [
-      bash
-      coreutils
-      gnugrep
-      util-linux
-      systemd
-      gptfdisk
-      gawk
-      dosfstools
-      btrfs-progs
-      cryptsetup
-      nixos-install-tools
-      gnused
-      zfs
-    ];
-
     # A shell script to run during the install phase
     installPhase = ''
       install -Dm755 nixos-install-script.sh $out/bin/nixos-install-script
-      wrapProgram $out/bin/nixos-install-script --prefix PATH : '${makeBinPath buildInputs}'
     '';
   }
